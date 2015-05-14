e6449ee3a2d3429c0d9fe217a8a6ab076c28b1ae
WilliamCipriano <Github@wfc.help>
2015-05-14 10:36:48 -0400

    Update OD-Appointment-Sender.py

--- OD-Appointment-Sender.py
+++ OD-Appointment-Sender.py
@@ -3,25 +3,27 @@ from datetime import datetime, timedelta
 import time
 import text
 import time
+import log
 
 #Written by: Will Cipriano, willcipriano.com
 
 #this accesses the OpenDental database and then loads all the patients with appointments for today, then uses text.msg() to send them to the respective phone numbers.
 #This depends on the twillo python module as well as mysql.connector.
 
+#Variables
 delta24h = timedelta(hours=13)
-
 date = time.strftime("%Y-%m-%d")
+patient_cell_number = []
+patient_appointment_time = []
 
 #Will not send reminders to patients scheduled before this time.
 reminder_hour = 9
-
-
-
-log = ''
+#How many seconds to wait before closing the program.
+wait_on_close = 30
+#How many seconds to wait between messages.
+send_delay = 1
 
 #Define database location and information here, if you are paranoid you can compile this to a .pyc to prevent a casual attacker.
-
 config = {
   'user': 'root',
   'password': '',
@@ -29,20 +31,14 @@ config = {
   'database': 'opendental',
   'raise_on_warnings': True,
 }
-
-
-
-patient_cell_number = []
-patient_appointment_time = []
-
 cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor()
 
-log += "\n Begining to send appointment reminders for: " + date + " \n"
+log.write("\n Beginning to send appointment reminders for: " + date, file = 'Send-Log.html')
 
 query = ("SELECT appointment.PatNum as Number, AptDateTime as Time, patient.WirelessPhone as Phone, patient.TxtMsgOk as Txt FROM appointment INNER JOIN patient on appointment.PatNum=patient.PatNum WHERE DATE(AptDateTime) = DATE('" + date + "') AND AptStatus = 1 GROUP BY Time;")
 cursor.execute(query)
-print "Patients Aquired"
+print "Patients Acquired"
 
 for patient in cursor:
     if patient[3] == 1:
@@ -51,21 +47,21 @@ for patient in cursor:
                 patient_cell_number.append(patient[2])
                 patient_appointment_time.append(patient[1])
 
-				
+
 x = 0
 for number in patient_cell_number:
     appt_time = patient_appointment_time[x].strftime('%I:%M %p')
-    log += "Reminder Sent - Cell Number: " + number + " Time: " + patient_appointment_time[x].strftime('%I:%M %p') + " \n"
-    msg = "This is a reminder of your appointment today scheduled for " + appt_time + " at Liberty Place 1625 Chestnut St. If you have any questions please don't hesitate to call us at (215)336-8399."
+    msg = "This is a reminder of your appointment today scheduled for " + appt_time + " at Liberty Place 1625 Chestnut St. Please text back Yes or call us at (215)336-8399 to confirm."
     print 'Sending reminder # ' + str(x + 1) + " of " + str(len(patient_cell_number))
+    try:
+        text.msg(number, msg)
+        log.write("Reminder Sent - Cell Number: " + number + " Time: " + patient_appointment_time[x].strftime('%I:%M %p'), file = 'Send-Log.html')
+    except Exception as ex:
+        log.write("Reminder Failure - Cell Number: " + number + " Time: " + patient_appointment_time[x].strftime('%I:%M %p') + ' Exception: ' + ex, file = 'Send-Log.html')
+
+#Wait a second between requests to be nice to twilio
+    time.sleep(send_delay)
     x += 1
-print "Patient reminders sent"    
-f = open('log.ini', 'a')
-print "Logfile open"
-log += "Appointment reminders sent successfully \n"
-f.write(log)
-print "Logs written"
-f.close()
-print "Logfile closed"
-print "Patient reminders have been sent"
-time.sleep(15)
+print "Patient reminders sent"
+log.write(str(x) + ' patient reminders have been sent. Waiting ', file = 'Overall.html')
+time.sleep(wait_on_close)
