6b1a10f27799c5c780870f1c8a87e77c65575ddd
WilliamCipriano <Github@wfc.help>
2015-05-20 14:29:56 -0400

    Clean up

--- listen.py
+++ listen.py
@@ -46,8 +46,11 @@ def text_responder():
 
     num = fromnum[2:]
     num = '(' + num[:3] + ')' + num[3:6] + '-' + num[6:10]
+    
+#Sorry this is a mess, I need to make this more pythonic later.
 
     if CheckForYes(body): 
+    #Do this if the patient confirms
         try:
             #see if patient exists
             query = ("SELECT AptNum, patient.PatNum, patient.WirelessPhone FROM appointment INNER JOIN patient on appointment.PatNum=patient.PatNum WHERE date(AptDateTime) = CURDATE() and WirelessPhone = '" + str(num) + "'")
@@ -60,6 +63,7 @@ def text_responder():
             confirmnote = "Confirmed automatically at: " + datetime.now().strftime('%I:%M %p')
             query = ("UPDATE appointment SET Confirmed = 21 WHERE AptNum = " + str(result[0]))
             cursor.execute(query)
+            #update the patients notes to reflect the conformation and the time
             query = ("UPDATE appointment SET note = concat(note, '\n" + confirmnote + "') WHERE AptNum=" + str(result[0]))
             cursor.execute(query)
             message = "Your appointment has been confirmed."
@@ -67,6 +71,7 @@ def text_responder():
             log.write('Database Error:' + str(ex), file = 'Recived-Log.html')
         log.write('Appointment confirmed for patient number: ' + str(result[1]), file = 'Recived-Log.html')
     else:
+    #Do this if the patient fails to confirm (IE. sends 'nope')
         try:
         #see if patient exists
             query = ("SELECT AptNum, patient.PatNum, patient.WirelessPhone FROM appointment INNER JOIN patient on appointment.PatNum=patient.PatNum WHERE date(AptDateTime) = CURDATE() and WirelessPhone = '" + str(num) + "'")
@@ -74,15 +79,15 @@ def text_responder():
             result = cursor.fetchone()
         except Exception as ex:
             log.write('Database Error:' + str(ex), file = 'Recived-Log.html')
-
         try:
+            #Make a note that the patient failed to confirm, with the message body
             confirmnote = "Patient Failed to confirm on, " + datetime.now().strftime('%I:%M %p') + " with message: " + body
             query = ("UPDATE appointment SET note = concat(note, '\n" + confirmnote + "') WHERE AptNum=" + str(result[0]))
             cursor.execute(query)
         except Exception as ex:
             log.write('Database Error:' + str(ex), file = 'Recived-Log.html')
 
-        message = "I'm sorry, I didn't get that. Please send Yes to confirm or call to cancel."
+        message = "I'm sorry, I didn't understand that. I'll forward your message to the staff but you can still send 'Yes' to confirm or call us to cancel."
         log.write('Confirm failed with message: ' + body, file = 'Recived-Log.html')
         
     resp = twilio.twiml.Response()
